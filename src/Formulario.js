3d768c8557a1592afbe7eeeb6e5c741f0497e57b
Willjunger <williamjunger@hotmail.com>
2020-02-16 20:50:43 -0300

    Validacao de form vazio

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import FormValidator from "./FormValidator";
 
 class Formulario extends Component {
 	constructor(props) {
 		super(props);
+
+		this.validador = new FormValidator({
+			campo: "nome",
+			metodo: "isEmpty"
+		});
+
 		this.stateInicial = {
 			nome: "",
 			livro: "",
 			preco: ""
 		};
+
 		this.state = this.stateInicial;
 	}
 
@@ -20,9 +28,10 @@ class Formulario extends Component {
 	};
 
 	submitFormulario = () => {
-		this.props.escutadorDeSubmit(this.state);
-
-		this.setState(this.stateInicial);
+		if (this.validador.valida(this.state)) {
+			this.props.escutadorDeSubmit(this.state);
+			this.setState(this.stateInicial);
+		}
 	};
 
 	render() {
